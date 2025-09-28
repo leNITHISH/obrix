3a970b012cdbc13aeff8a08b49f8362525de285b
Nithish Siva Kumar <the.nithish.siva.kumar@gmail.com>
2025-09-28 15:35:14 +0530

    fixed pin not showing up

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'leaflet/dist/leaflet.css'
 import './index.css'
-import { AuthProvider } from './context/AuthContext' // <-- Make sure this is imported
+import { AuthProvider } from './context/AuthContext'
+
+// --- START: Fix for Leaflet's missing marker icons in production ---
+import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+// --- END OF FIX ---
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider> {/* <-- This wrapper is essential */}
+    <AuthProvider>
       <App />
     </AuthProvider>
   </React.StrictMode>,
